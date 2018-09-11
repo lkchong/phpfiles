0ae617cc18ba284abe0a179cd71551484ab12da2
lkchong <lkchong91@gmail.com>
2018-09-11 18:55:58 +0800

    First commit

---
+++ login.php
@@ -0,0 +1,12 @@
+<?php
+require "conn.php";
+$user_name = $_POST["user"];
+$user_pass = $_POST["pass"];
+$mysql_qry = "select * from customer  where username like '$user_name' and password like '$user_pass'";
+$result = mysqli_query($conn, $mysql_qry);
+if(mysqli_num_rows($result) > 0) {
+	echo "login successful";
+}
+else
+	echo "login not successful";
+?>
