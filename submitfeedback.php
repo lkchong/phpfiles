56300d5606b034acbda88476982252e2ed57a659
lkchong <lkchong91@gmail.com>
2018-10-07 11:30:40 +0800

    submitfeedback php updated

--- submitfeedback.php
+++ submitfeedback.php
@@ -6,14 +6,11 @@
 	$feedback_rating = $_POST["feedbackRating"];
 	$feedback_details = $_POST["feedbackDetails"];
 
-
-	$mysql_qry = "CALL Submit_Feedback($custID, $feedback_category, $feedback_rating, $feedback_details)";
+	$mysql_qry = "CALL Submit_Feedback('$custID', '$feedback_category', '$feedback_rating', '$feedback_details')";
 
 	$result = mysqli_query($conn, $mysql_qry);
-	if(mysqli_num_rows($result) > 0) {
-		while($row = mysqli_fetch_assoc($result)) {
-			echo $row['cust_id'];
-		}
+	if($result) {
+		echo "Success";
 	}
 	else {
 		echo "Failed";
